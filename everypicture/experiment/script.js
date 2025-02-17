06a79430bb05abc30bc9c3b95e20418109ccfa6d
Victor Barajas <victorbarajas@Victors-MacBook-Air-7.local>
2025-02-17 11:11:35 -0800

    exp

--- everypicture/experiment/script.js
+++ everypicture/experiment/script.js
@@ -1,3 +1,26 @@
 (function(){
-    
-}())
+    'use strict';
+    console.log('reading js');
+
+    const container = document.querySelector('#container');
+    const spots = document.querySelectorAll('#container div');
+    const pic = document.querySelector('div img');
+    const hider = document.querySelector('#hideme');
+
+
+    spots.forEach(function(eachSpot){
+        eachSpot.addEventListener('mouseover', effect);
+    });
+
+    function effect(event){
+        const thisCorner = event.target.id;
+        console.log(thisCorner);
+        switch (thisCorner){
+            case 'topl': pic.className = 'topl'; break;
+            case 'topr': pic.className = 'topr'; break;
+            case 'botl': pic.className = 'botl'; break;
+            case 'botr': pic.className = 'botr'; break;
+            case 'center': pic.className = 'center'; break;
+        };
+    };
+}());
